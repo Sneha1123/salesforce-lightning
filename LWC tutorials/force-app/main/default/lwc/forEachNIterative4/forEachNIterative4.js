e90e11e1e69be71ee0115189eac371b9cf5a581c
Sneha <sneha.941123@gmail.com>
2020-04-29 15:13:46 +0530

    29-04-2020 - simple interest calculator added

---
+++ LWC tutorials/force-app/main/default/lwc/forEachNIterative4/forEachNIterative4.js
@@ -0,0 +1,22 @@
+import { LightningElement } from 'lwc';
+
+export default class ForEachNIterative4 extends LightningElement {
+
+    contacts = [
+        {
+            "id":"1",
+            "name":"a",
+            "title":"vp"
+        },
+        {
+            "id":"2",
+            "name":"b",
+            "title":"ceo"
+        },
+        {
+            "id":"3",
+            "name":"c",
+            "title":"hod"
+        }
+    ]
+}
---
+++ LWC tutorials/force-app/main/default/lwc/simpleInterestCalculator5/simpleInterestCalculator5.js
@@ -0,0 +1,30 @@
+import { LightningElement,track } from 'lwc';
+
+export default class SimpleInterestCalculator5 extends LightningElement {
+    @track principal;
+    @track roi;
+    @track time;
+    @track showValue=false;
+    @track si;
+
+    principalhandler(event){
+
+        this.principal=event.target.value;
+    }
+    roihandler(event){
+
+        this.roi=event.target.value;
+    }
+    timehandler(event){
+
+        this.time=event.target.value;
+    }
+
+    calculateHandler(event){
+        this.si = ((this.principal)*(this.roi)*(this.time))/100;
+        this.showValue = true;
+
+
+        
+    }
+}
